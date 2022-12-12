024b2819195cf45ca8d62003029fa35c797f950d
hector <hectorpelaez11@hotmail.com>
2022-12-12 16:56:21 -0500

    primer commit

---
+++ BackEnd/goliath_national_bank/src/main/java/com/goliath_national_bank/goliath_national_bank/GoliathNationalBankApplication.java
@@ -0,0 +1,13 @@
+package com.goliath_national_bank.goliath_national_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoliathNationalBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoliathNationalBankApplication.class, args);
+	}
+
+}
---
+++ BackEnd/goliath_national_bank/src/test/java/com/goliath_national_bank/goliath_national_bank/GoliathNationalBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goliath_national_bank.goliath_national_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoliathNationalBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
